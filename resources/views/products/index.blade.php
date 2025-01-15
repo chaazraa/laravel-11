af5170a1d1d7b5b077b94a5f683837bdeae10657
achaa <hafshahazzahraa@gmail.com>
2025-01-15 09:13:12 +0700

    yey

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Products - SantriKoding.com</title>
+    <title>Data Products - tokocaa.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
@@ -13,8 +13,8 @@
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
+                    <h3 class="text-center my-4">Welcome to the fun side of shopping!</h3>
+                    <h5 class="text-center"><a href="https://santrikoding.com">www.tokocaa.com</a></h5>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
